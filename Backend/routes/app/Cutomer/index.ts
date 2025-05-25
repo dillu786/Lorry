e73d1471d03cbb7a1f029d4a7a100fa0000e1b4d
Dilshad <azam.dilshad@gmail.com>
2025-05-26 01:39:40 +0530

    added customer routes

--- Backend/routes/app/Cutomer/index.ts
+++ Backend/routes/app/Cutomer/index.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import bookingRoutes from "./booking"
 import authRoutes from "./auth"
+import { getCustomerDetails } from "../../../controllers/app/Customer/booking";
+import { CustomerMiddleware } from "../../../middlewares/middleware";
 
 const router = express.Router();
 
 router.use("/booking",bookingRoutes);
 router.use("/auth",authRoutes);
 
+router.get("/customerDetails",CustomerMiddleware,getCustomerDetails);
 export default router;
