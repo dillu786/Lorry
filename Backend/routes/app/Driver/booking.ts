1e68ccb9eebb9b72ea0befc9bf1a55e12e20fa26
Dilshad <azam.dilshad@gmail.com>
2025-06-08 22:50:09 +0530

    getDriverBooking History

--- Backend/routes/app/Driver/booking.ts
+++ Backend/routes/app/Driver/booking.ts
@@ -1,7 +1,7 @@
 import express from "express"
 import { Drivermiddleware } from "../../../middlewares/middleware";
 import multer from "multer"
-import { acceptedBookings, acceptRide, onGoingRide, completedRides, startTrip, makeDriverOnline, makeDriverOffline, negotiateFare, newBookings, endTrip  } from "../../../controllers/app/Driver/booking";
+import { acceptedBookings, acceptRide, onGoingRide, completedRides, startTrip, makeDriverOnline, makeDriverOffline, negotiateFare, newBookings, endTrip, getDriverBookingHistory  } from "../../../controllers/app/Driver/booking";
 import { getUserBookingHistory } from "../../../controllers/app/Customer/booking";
 const router = express.Router();
 const storage = multer.memoryStorage()
@@ -13,7 +13,7 @@ router.post("/startTrip",Drivermiddleware,upload.fields([{
 ,startTrip);
 router.post("/acceptRides",Drivermiddleware,acceptRide);
 router.get("/accetedBooking",Drivermiddleware,acceptedBookings);
-router.get("/historicalRides",Drivermiddleware,getUserBookingHistory);
+router.get("/historicalRides",Drivermiddleware,getDriverBookingHistory);
 router.get("/ongoingRide",Drivermiddleware,onGoingRide);
 router.get("/completedRides",Drivermiddleware,completedRides);
 router.patch("/makeDriverOnline",Drivermiddleware,makeDriverOnline);
